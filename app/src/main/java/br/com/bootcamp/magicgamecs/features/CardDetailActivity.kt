50ff8691b93b711b32c5316c158c5e8f66bc7d3d
Thiago Matheus <thiagomatheusms@gmail.com>
2019-09-17 15:28:03 -0300

    Card detail layout

---
+++ app/src/main/java/br/com/bootcamp/magicgamecs/features/CardDetailActivity.kt
@@ -0,0 +1,13 @@
+package br.com.bootcamp.magicgamecs.features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.bootcamp.magicgamecs.R
+
+class CardDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_detail)
+    }
+}
