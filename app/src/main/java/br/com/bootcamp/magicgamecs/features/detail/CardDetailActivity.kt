3c3d0a6ac646750adc3fb6ba022cad4757ed16e0
Thiago Matheus <thiagomatheusms@gmail.com>
2019-09-25 08:54:02 -0300

    Detail card with text - fix

--- app/src/main/java/br/com/bootcamp/magicgamecs/features/detail/CardDetailActivity.kt
+++ app/src/main/java/br/com/bootcamp/magicgamecs/features/detail/CardDetailActivity.kt
@@ -3,9 +3,15 @@ package br.com.bootcamp.magicgamecs.features
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.bootcamp.magicgamecs.R
+import br.com.bootcamp.magicgamecs.features.home.CardImageRequestListener
 import br.com.bootcamp.magicgamecs.models.pojo.Card
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_card_detail.*
+import kotlinx.android.synthetic.main.item_main_activity_card.*
+import kotlinx.android.synthetic.main.item_main_activity_card.view.*
+import kotlinx.android.synthetic.main.item_main_activity_card.view.imageView_card_item
+import kotlinx.android.synthetic.main.item_main_activity_card.view.shimmer_card
+import kotlinx.android.synthetic.main.item_main_activity_card.view.text_card_placeholder
 
 class CardDetailActivity : AppCompatActivity() {
 
@@ -20,21 +26,18 @@ class CardDetailActivity : AppCompatActivity() {
             .load(card.imageUrl)
             .placeholder(R.drawable.placeholder_card)
             .error(R.drawable.placeholder_card)
-            //.transform(RoundedCorners(10))
-            .into(imageView_card_detail)
+            .addListener(
+                CardImageRequestListener(
+                    card,
+                    shimmer_card,
+                    text_card_placeholder
+                )
+            )
+            .into(imageView_card_item)
 
         imageView_close_detail.setOnClickListener {
             finish()
         }
 
-//        val imageCarousel : CarouselPicker = findViewById(R.id.carousel)
-//
-//        val imageItems = ArrayList<CarouselPicker.PickerItem>()
-//        imageItems.add(CarouselPicker.DrawableItem(R.drawable.placeholder_card))
-//        imageItems.add(CarouselPicker.DrawableItem(R.drawable.placeholder_card))
-//        imageItems.add(CarouselPicker.DrawableItem(R.drawable.placeholder_card))
-//        imageItems.add(CarouselPicker.DrawableItem(R.drawable.fundo))
-//        val imageAdapter = CarouselPicker.CarouselViewAdapter(this, imageItems, 0)
-//        imageCarousel.setAdapter(imageAdapter)
     }
 }
