a4a3a4965f572c7bfbfb300108f3f70e15381059
Thiago Matheus <thiagomatheusms@gmail.com>
2019-09-30 10:03:27 -0300

    Delete MagicRepositoryDb

--- app/src/androidTest/java/br/com/bootcamp/magicgamecs/features/home/MainActivityTest.kt
+++ app/src/androidTest/java/br/com/bootcamp/magicgamecs/features/home/MainActivityTest.kt
@@ -1,5 +1,25 @@
 package br.com.bootcamp.magicgamecs.features.home
 
-import org.junit.Assert.*
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import br.com.bootcamp.magicgamecs.R
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
-class MainActivityTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+
+    @Test
+    fun testRecyclerViewClick() {
+        onView(withId(R.id.recyclerView_main)).perform(RecyclerViewActions.actionOnItemAtPosition<CollectionAdapter.CollectionViewHolder>(0, click()))
+    }
+}
